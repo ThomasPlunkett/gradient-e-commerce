d4cc58c4e9ca180b7c16025e4cfa9332da5fe401
Ian F <ian.ferg36@gmail.com>
2021-05-25 08:35:07 -0600

    reattached react router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-<<<<<<< HEAD
+import logo from "./assets/46.png";
 import "./App.css";
-import Header from "./components/Header/Header";
-=======
-import logo from './assets/46.png';
-import './App.css';
->>>>>>> 2ff234cb55b6c0f64b1fe135101e4b1e0f16b0ea
 
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </Header>
-=======
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        
-        
       </header>
 
       <Footer />
->>>>>>> 2ff234cb55b6c0f64b1fe135101e4b1e0f16b0ea
     </div>
   );
 }
