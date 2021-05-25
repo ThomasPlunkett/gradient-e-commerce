bbf7a1e2272b5ff1b400f8fe3267ef60d4ddd132
Ian F <ian.ferg36@gmail.com>
2021-05-24 18:25:16 -0600

    major debugging, changed some alignment from template css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <nav className="nav-menu container">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+      </ul>
+      <div className="logo">
+        <Link to="/">Cart</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
