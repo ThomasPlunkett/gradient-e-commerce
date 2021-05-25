241d465051faa39c3a803a217f31781cfd0c3c5a
Ian F <ian.ferg36@gmail.com>
2021-05-25 08:40:34 -0600

    fixed issue with header import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from "./assets/46.png";
 import "./App.css";
-
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      </header>
+      <Header />
 
       <Footer />
     </div>
