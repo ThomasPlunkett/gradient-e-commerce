3e0f6f528cbd638c45e4f93a1cd8ca9dbc5bbcf9
Ian F <ian.ferg36@gmail.com>
2021-05-24 19:16:41 -0600

    added cart icon with hardcoded quantity

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.scss";
+import CartIcon from "../CartIcon/Carticon";
 
 const Header = () => {
   return (
     <nav className="nav-menu container">
       <ul>
         <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/">Shop</Link>
+          <Link to="/">Store</Link>
         </li>
       </ul>
-      <div className="logo">
-        <Link to="/">Cart</Link>
+      <div className="center-nav">
+        <div>
+          <Link to="/"> Shop </Link>
+        </div>
+        <div>
+          <div>
+            <Link to="/"> Home </Link>
+          </div>
+        </div>
       </div>
+
+      <CartIcon />
     </nav>
   );
 };
