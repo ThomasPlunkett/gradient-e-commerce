8d561ee268f8f4c3cf5694f717c6c7c23dd2801e
Ian F <ian.ferg36@gmail.com>
2021-05-26 17:11:26 -0600

    routed to shop page if  product id is not found

--- src/components/Single-Product/Single-Product.js
+++ src/components/Single-Product/Single-Product.js
@@ -1,4 +1,21 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
 import { ProductsContext } from "../../context/Products-Context";
 import Layout from "../shared/Layout";
 import "./SingleProduct.scss";
+
+const SingleProduct = ({ match, history: { push } }) => {
+  const { products } = useContext(ProductsContext);
+  const { id } = match.params;
+  const [product, setProduct] = useState(null);
+  useEffect(() => {
+    const product = products.find((item) => Number(item.id) === Number(id));
+
+    if (!product) {
+      //    this route name is just what the instructions said, just make sure it's routed to Emily's product/shop page
+      return push("/shop");
+    }
+
+    setProduct(product);
+  });
+};
