3f12119213af0b286212ab25c43099f431c4f5c2
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-28 11:35:46 -0400

    added route handling

--- src/components/CartIcon/Carticon.js
+++ src/components/CartIcon/Carticon.js
@@ -1,20 +1,21 @@
-import React, { useContext } from "react";
-import cartImg from "../../assets/shopping-cart (3).png";
-import { CartContext } from "../../context/cart-context";
-import "./CartIcon.scss";
+import React, { useContext } from 'react';
+import { withRouter } from 'react-router-dom';
+import shoppingBag from '../../assets/shopping-bag.png';
+import { CartContext } from '../../context/cart-context';
+import './cart-icon.styles.scss';
 
-const CartIcon = () => {
+const CartIcon = ({ history }) => {
   const { itemCount, cartItems } = useContext(CartContext);
-  console.log('CartItems', cartItems);
+  console.log('CartItems:', cartItems);
   return (
-    <div className="cart-container">
-      <img src={cartImg} alt="shopping cart png"></img>
+    <div className='cart-container' onClick={() => history.push('/cart')}>
+      <img src={shoppingBag} alt='shopping-cart-icon' />
       {
-        itemCount > 0 ? <span className="cart-count"> { itemCount } </span>: null
+        itemCount > 0 ? <span className='cart-count'> { itemCount } </span> : null
       }
       
     </div>
   );
-};
+}
 
-export default CartIcon;
+export default withRouter(CartIcon);
