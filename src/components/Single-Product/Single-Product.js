021e751e960ef7935dfcfaee390e58b3ce459166
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-28 10:43:11 -0400

    Increase Item Quantity
    modified code in single products.js to give more functionality to Add to Cart button - can now Add More items to Cart

--- src/components/Single-Product/Single-Product.js
+++ src/components/Single-Product/Single-Product.js
@@ -1,17 +1,21 @@
 import React, { useContext, useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { ProductsContext } from "../../context/Products-Context";
+import { CartContext } from '../../context/cart-context';
+import { inInCart } from '../../helpers';
 // Couldn't remember if we were sticking with the layout component or not, just imported it to be safe, it's used in the jsx as well but we can always swap it out no problem if not
 import Layout from "../shared/Layout";
 import "./SingleProduct.scss";
 
 const SingleProduct = ({ match, history: { push } }) => {
   const { products } = useContext(ProductsContext);
+  const { addProduct, cartItems } = useContext(CartContext);
   const { id } = match.params;
   const [product, setProduct] = useState(null);
   useEffect(() => {
     const product = products.find((item) => Number(item.id) === Number(id));
-
+    
+    // if product does not exist, redirect to shop page
     if (!product) {
       //    this route name is just what the instructions said, just make sure it's routed to Emily's product/shop page
       return push("/shop");
@@ -19,8 +23,10 @@ const SingleProduct = ({ match, history: { push } }) => {
 
     setProduct(product);
   }, [id, product, push, products]);
-
+  // while we check for product
+  if (!product) { return null }
   const { imageUrl, title, price, description } = product;
+  const itemInCart = inInCart(product, cartItems);
   return (
     <Layout>
       <div className="single-product-container">
@@ -33,9 +39,24 @@ const SingleProduct = ({ match, history: { push } }) => {
             <p>{price}</p>
           </div>
           <div className="add-to-cart-buttons">
-            <button className="button is-white nomad-btn" id="btn-gray-outline">
-              Add to Cart
-            </button>
+            {
+              !inInCart && 
+              <button 
+                className="button is-white nomad-btn" 
+                id="btn-gray-outline"
+                onClick={() => addProduct(product)}>
+                  Add to Cart
+              </button>
+            }
+            {
+              isInCart &&
+              <button 
+                className="button is-white nomad-btn" 
+                id="btn-gray-outline"
+                onClick={()=> addProduct(product)}>
+                  Add More
+              </button>
+            }
             <button
               className="button is-black nomad-btn"
               id="btn-white-outline"
