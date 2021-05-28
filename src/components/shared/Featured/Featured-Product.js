b9fffd5ee75b4c2099619d0dc71519718d0d5239
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-27 23:14:12 -0400

    Add Click Handler To Add Product To Cart

--- src/components/shared/Featured/Featured-Product.js
+++ src/components/shared/Featured/Featured-Product.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { isInCart } from '../../helpers';
+import { CartContext } from '../../context/cart-context';
 import { withRouter } from "react-router-dom";
 import "./Featured.scss";
 
 const FeaturedProduct = (props) => {
   const { id, title, imageUrl, price, history } = props;
-
+  const product = { id, title, imageUrl, price, history };
+  const { addProduct, cartItems } = useContext(CartContext);
   return (
     <div className="featured">
       <div
         className="featured-image"
-        onClick={() => history.push(`/product/${id}`)}
-      >
+        onClick={() => history.push(`/product/${id}`)}>
         <img src={imageUrl} alt="product" />
       </div>
       <div className="name-price">
         <h3> {title}</h3>
         <p> ${price}</p>
-        <button className="button is-black nomad-btn">Add to Cart</button>
+        {
+          !inInCart(product, cartItems) && 
+          <button 
+            className="button is-black nomad-btn"
+            onClick={() => addProduct(product)}>
+              Add to Cart</button>
+        }
+        {
+          !inInCart(product, cartItems) &&
+          <button 
+            className="button is-black nomad-btn"
+            id='btn-white-outline'
+            onClick={()=> {}}>
+              Add More </button>
+        }
       </div>
     </div>
   );
